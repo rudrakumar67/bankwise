f6d8c3a1e37d00e47b52bbd94454ee0a9028cc97
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-10-13 17:38:30 +0530

    changes

--- src/app/common/NevBar.tsx
+++ src/app/common/NevBar.tsx
@@ -6,45 +6,47 @@ import {
 
 function Navbar() {
     return (
-    <header className="z-10 py-2 border-b border-gray-200">
-      <div className="wrapper flex items-center justify-between h-full px-6 mx-auto">
-        <div className="flex justify-between flex-0 h-full w-96">
-          <div className="relative w-full max-w-xl border-2 border-gray-200 ">
-            <div className="absolute inset-y-0 flex items-center pl-2">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                />
-              </svg>
+      <>
+      <header className="z-10 py-3 border-b border-gray-200">
+        <div className="wrapper flex items-center justify-between h-full px-6 mx-auto">
+          <div className="flex justify-between flex-0 h-full w-96">
+            <div className="relative w-full max-w-xl border-2 border-gray-200 ">
+              <div className="absolute inset-y-0 flex items-center pl-2">
+                <svg
+                  className="w-4 h-4"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <input
+                className="w-full h-full pl-8 pr-2 pb-1.5 focus:outline-none placeholder:text-xs"
+                type="text"
+                placeholder="Search for projects"
+                aria-label="Search"
+              />
             </div>
-            <input
-              className="w-full h-full pl-8 pr-2 pb-1.5 focus:outline-none placeholder:text-xs"
-              type="text"
-              placeholder="Search for projects"
-              aria-label="Search"
-            />
           </div>
+          <ul className="flex items-center flex-shrink-0 space-x-5">
+            <a href="" className='border-2 rounded p-2 border-gray-200'>
+              <EnvelopeIcon className='h-4 w-4 text-blue-500'/>
+            </a>
+            <a href="" className='border-2 rounded p-2 border-gray-200'>
+              <BellAlertIcon className='h-4 w-4 text-blue-500'/>
+            </a>
+            <a href="" className='bg-blue-900 rounded p-2 border-2 border-blue-900'>
+              <UserIcon className='h-4 w-4 text-slate-50'/>
+            </a>
+          </ul>
         </div>
-        <ul className="flex items-center flex-shrink-0 space-x-5">
-          <a href="" className='border-2 rounded p-2 border-gray-200'>
-            <EnvelopeIcon className='h-4 w-4 text-blue-500'/>
-          </a>
-          <a href="" className='border-2 rounded p-2 border-gray-200'>
-            <BellAlertIcon className='h-4 w-4 text-blue-500'/>
-          </a>
-          <a href="" className='bg-blue-900 rounded p-2 border-2 border-blue-900'>
-            <UserIcon className='h-4 w-4 text-slate-50'/>
-          </a>
-        </ul>
-      </div>
-    </header>
+      </header>
+    </>
     );
   }
   
--- src/app/lead/personal-loan/page.tsx
+++ src/app/lead/personal-loan/page.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link"
-import { ArrowRightCircleIcon , PlusCircleIcon,ArchiveBoxArrowDownIcon, TrashIcon, ArrowDownTrayIcon} from "@heroicons/react/24/outline"
+import { ArrowRightCircleIcon , PlusCircleIcon,ArchiveBoxArrowDownIcon, TrashIcon, ArrowDownTrayIcon, ChevronRightIcon, ShareIcon, UserPlusIcon} from "@heroicons/react/24/outline"
 
 export default function Home() {
   return (
     <>
         <div className="wrapper">
-            <div className="grid gap-4 mb-8 md:grid-cols-6 xl:grid-cols-6 py-4 ">
+            <div className="flex h-6 justify-between my-1">
+                <div className='text-black font-semibold'>
+                Personal Loan
+                </div>
+                <div className='flex text-black text-xs my-auto'>
+                Home <ChevronRightIcon className='h-2.5 w-2.5 my-auto mx-1' /> 
+                Lead <ChevronRightIcon className='h-2.5 w-2.5 my-auto mx-1' /> 
+                Personal Loan <ChevronRightIcon className='h-2.5 w-2.5 my-auto mx-1' /> List
+                </div>
+            </div>
+            <div className="grid gap-4 mb-4 md:grid-cols-6 xl:grid-cols-6 py-2 ">
                 <Link href='/lead/personal-loan'>
                     <div className="flex items-center h-12 border bg-indigo-100 border-slate-300 rounded shadow-xs">
                         <div className="p-2 md:p-1 m-3 rounded">
@@ -73,7 +83,7 @@ export default function Home() {
                         </div>
                         <div>
                             <p className="my-2 pd:my-1 md:text-xs text-sm font-medium text-gray-600">
-                                Statment Analyzer
+                                Import Website Leads
                             </p>
                         </div>
                     </div>
@@ -81,8 +91,19 @@ export default function Home() {
             </div>
         </div>
         <div className="flex justify-between">
-            <div className="text-black text-xl">
-                Personal Loan
+            <div className="text-black flex">
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                    <ArrowDownTrayIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                    <span className="text-sm font-semibold">Import Data</span>
+                </Link>
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                    <ShareIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                    <span className="text-sm font-semibold">Share Link</span>
+                </Link>
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                    <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                    <span className="text-sm font-semibold">Assign To</span>
+                </Link>
             </div>
             <div className="flex h-8">
                 <Link href="/" className="flex text-white py-1.5 px-3 rounded primary-btn mr-2">
@@ -96,10 +117,6 @@ export default function Home() {
                 <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
                     <TrashIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
                     <span className="text-sm font-semibold">Delete</span>
-                </Link>
-                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
-                    <ArrowDownTrayIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                    <span className="text-sm font-semibold">Import Data</span>
                 </Link>
             </div>
         </div>
