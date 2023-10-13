1391905c1d1a5126ab282e152f73bfb088fdc2cb
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-10-13 11:40:51 +0530

    change

--- src/app/common/SideBar.tsx
+++ src/app/common/SideBar.tsx
@@ -19,114 +19,103 @@ function Sidebar() {
             <div className="py-4 text-gray-500">
               <a className="font-medium px-1.5 font-sm capitalize" style={{color:"#e67300"}} href="#">Bankwise</a>
               <ul className='mt-4'>
-                {/* <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className={"flex flex-col py-1.5 md:py-1" + (pathname == '/' ? "active" : "")}>
+                <li className="relative px-3 py-1">
+                    <div className={"flex flex-col py-1.5 " + (pathname == '/' ? "active" : "")}>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <ChartBarIcon className={"h-6 w-6 md:h-5 md:w-5 "}/>
+                        <ChartBarIcon className={"h-6 w-6 "} color="{'pathname == '/' ? '#12344d' : ''}"/>
                         </a>
-                        <span style={{color:'#12344d'}} className='text-sm md:text-xs text-center font-semibold'>Dashboard</span>
-                    </div>
-                </li> */}
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
-                        <a
-                            className="inline-flex items-center w-full menu-item justify-center"
-                            href="forms.html"
-                        >
-                        <FunnelIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/leads' ? "" : "")} color='#ffffff'/>
-                        </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Leads</span>
+                        <span style={{fontSize:'10px', color:'#12344d'}} className='text-center font-semibold'>Dashboard</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <FunnelIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/leads' ? "" : "")} color='#ffffff'/>
+                        <FunnelIcon className={"h-6 w-6 " + (pathname == '/leads' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Leads</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>Leads</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <LinkIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
+                        <LinkIcon className={"h-6 w-6 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Links</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>Links</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <ReceiptRefundIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
+                        <ReceiptRefundIcon className={"h-6 w-6 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Re-Target</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>Re-Target</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <UserGroupIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
+                        <UserGroupIcon className={"h-6 w-6 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Teams</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>Teams</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <ChartPieIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
+                        <ChartPieIcon className={"h-6 w-6 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Reports</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>Reports</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <CalculatorIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
+                        <CalculatorIcon className={"h-6 w-6 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Accounts</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>Accounts</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <UserIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
+                        <UserIcon className={"h-6 w-6 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>HR</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>HR</span>
                     </div>
                 </li>
-                <li className="relative px-3 py-1 md:px-1.5 md:py-0.5">
-                    <div className='flex flex-col py-1.5 md:py-1'>
+                <li className="relative px-3 py-1">
+                    <div className='flex flex-col py-1.5'>
                         <a
                             className="inline-flex items-center w-full menu-item justify-center"
                             href="forms.html"
                         >
-                        <WrenchScrewdriverIcon className={"h-6 w-6 md:h-5 md:w-5 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
+                        <WrenchScrewdriverIcon className={"h-6 w-6 " + (pathname == '/' ? "" : "")} color='#ffffff'/>
                         </a>
-                        <span style={{color:'#ffffff'}} className='text-sm text-center font-semibold'>Settings</span>
+                        <span style={{fontSize:'10px', color:'#ffffff'}} className='text-center font-semibold'>Settings</span>
                     </div>
                 </li>
               </ul>
