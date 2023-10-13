b3880325f5474e05c159c8a87cebf4bcce3e1f03
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-10-13 19:17:42 +0530

    changes

--- src/app/lead/personal-loan/page.tsx
+++ src/app/lead/personal-loan/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
                             <ArrowRightCircleIcon className={"h-6 w-6 md:h-4 md:w-4 text-slate-400"}/>
                         </div>
                         <div>
-                            <p className="my-2 pd:my-1 md:text-xs text-sm font-medium text-gray-600">
+                            <p className="my-2 pd:my-1 text-sm font-medium text-gray-600">
                                 EMI Calculator
                             </p>
                         </div>
@@ -33,7 +33,7 @@ export default function Home() {
                             <ArrowRightCircleIcon className={"h-6 w-6 md:h-4 md:w-4 text-slate-400"}/>
                         </div>
                         <div>
-                            <p className="my-2 pd:my-1 md:text-xs text-sm font-medium text-gray-600">
+                            <p className="my-2 pd:my-1 text-sm font-medium text-gray-600">
                                 Company Validator
                             </p>
                         </div>
@@ -45,7 +45,7 @@ export default function Home() {
                             <ArrowRightCircleIcon className={"h-6 w-6 md:h-4 md:w-4 text-slate-400"}/>
                         </div>
                         <div>
-                            <p className="my-2 pd:my-1 md:text-xs text-sm font-medium text-gray-600">
+                            <p className="my-2 pd:my-1 text-sm font-medium text-gray-600">
                                 Bank Criterias
                             </p>
                         </div>
@@ -57,7 +57,7 @@ export default function Home() {
                             <ArrowRightCircleIcon className={"h-6 w-6 md:h-4 md:w-4 text-slate-400"}/>
                         </div>
                         <div>
-                            <p className="my-2 pd:my-1 md:text-xs text-sm font-medium text-gray-600">
+                            <p className="my-2 pd:my-1 text-sm font-medium text-gray-600">
                                 Eligibility Checker
                             </p>
                         </div>
@@ -69,7 +69,7 @@ export default function Home() {
                             <ArrowRightCircleIcon className={"h-6 w-6 md:h-4 md:w-4 text-slate-400"}/>
                         </div>
                         <div>
-                            <p className="my-2 pd:my-1 md:text-xs text-sm font-medium text-gray-600">
+                            <p className="my-2 pd:my-1 text-sm font-medium text-gray-600">
                                 CIBIL Report
                             </p>
                         </div>
@@ -81,7 +81,7 @@ export default function Home() {
                             <ArrowRightCircleIcon className={"h-6 w-6 md:h-4 md:w-4 text-slate-400"}/>
                         </div>
                         <div>
-                            <p className="my-2 pd:my-1 md:text-xs text-sm font-medium text-gray-600">
+                            <p className="my-2 pd:my-1 text-sm font-medium text-gray-600">
                                 Import Website Leads
                             </p>
                         </div>
@@ -91,31 +91,31 @@ export default function Home() {
         </div>
         <div className="flex justify-between">
             <div className="text-black flex">
-                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 border border-gray-400 rounded shadow mr-2">
                     <ArrowDownTrayIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                    <span className="text-sm font-semibold">Import Data</span>
+                    <span className="text-sm md:text-xs font-medium my-auto">Import Data</span>
                 </Link>
-                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 border border-gray-400 rounded shadow mr-2">
                     <ShareIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                    <span className="text-sm font-semibold">Share Link</span>
+                    <span className="text-sm md:text-xs font-medium my-auto">Share Link</span>
                 </Link>
-                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 border border-gray-400 rounded shadow mr-2">
                     <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                    <span className="text-sm font-semibold">Assign To</span>
+                    <span className="text-sm md:text-xs font-medium my-auto">Assign To</span>
                 </Link>
             </div>
             <div className="flex h-8">
                 <Link href="/" className="flex text-white py-1.5 px-3 rounded primary-btn mr-2">
                     <PlusCircleIcon className={"h-4.5 w-4.5 text-slate-50 mr-1"}/>
-                    <span className="text-sm font-semibold">Add New</span>
+                    <span className="text-sm md:text-xs font-medium my-auto">Add New</span>
                 </Link>
-                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 border border-gray-400 rounded shadow mr-2">
                     <ArchiveBoxArrowDownIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                    <span className="text-sm font-semibold">Archive</span>
+                    <span className="text-sm md:text-xs font-medium my-auto">Archive</span>
                 </Link>
-                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow mr-2">
+                <Link href="/" className="flex bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 border border-gray-400 rounded shadow mr-2">
                     <TrashIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                    <span className="text-sm font-semibold">Delete</span>
+                    <span className="text-sm md:text-xs font-medium my-auto">Delete</span>
                 </Link>
             </div>
         </div>
