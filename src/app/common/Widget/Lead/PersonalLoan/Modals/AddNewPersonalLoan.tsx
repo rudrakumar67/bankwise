82567892909274d5200045ba03317216239dc6a2
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-10-20 20:51:28 +0530

    changes

--- src/app/common/Widget/Lead/PersonalLoan/Modals/AddNewPersonalLoan.tsx
+++ src/app/common/Widget/Lead/PersonalLoan/Modals/AddNewPersonalLoan.tsx
@@ -132,9 +132,9 @@ const Tabs = () => {
             </li>
           </ul>
           <div className="relative flex flex-col min-w-0 break-words bg-white text-black w-full mb-3 ">
-            <div className="py-2 flex-auto border p-3 rounded border-t-0">
-              <h4 className="text-left pb-3">Basic Details</h4>
-              <div className="tab-content tab-space">
+            <div className="flex-auto p-4 pb-6 bg-slate-100 rounded">
+              <h4 className="text-left pb-4 font-semibold">Basic Details</h4>
+              <div className="tab-content tab-space ">
                 <div className={openTab === 1 ? "block" : "hidden"} id="link1">
                   <div className="grid grid-cols-4 gap-4">
                     <div className="flex flex-col">
