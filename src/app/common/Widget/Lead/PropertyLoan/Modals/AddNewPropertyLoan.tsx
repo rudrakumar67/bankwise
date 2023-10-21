24e27ceebadf1ad80c9546f96850a31ad99a5f03
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-10-21 14:48:54 +0530

    changes

--- src/app/common/Widget/Lead/PropertyLoan/Modals/AddNewPropertyLoan.tsx
+++ src/app/common/Widget/Lead/PropertyLoan/Modals/AddNewPropertyLoan.tsx
@@ -247,6 +247,10 @@ const Tabs = () => {
                       <input type="text" name="" id=""  className="border rounded w-52 h-8" style={{borderColor:'#cfd7df'}}/>
                     </div>
                     <div className="flex flex-col">
+                      <label htmlFor="" className="text-left text-xs font-normal pb-1">City</label>
+                      <input type="text" name="" id=""  className="border rounded w-52 h-8" style={{borderColor:'#cfd7df'}}/>
+                    </div>
+                    <div className="flex flex-col">
                       <label htmlFor="" className="text-left text-xs font-normal pb-1">Location</label>
                       <input type="text" name="" id=""  className="border rounded w-52 h-8" style={{borderColor:'#cfd7df'}}/>
                     </div>
