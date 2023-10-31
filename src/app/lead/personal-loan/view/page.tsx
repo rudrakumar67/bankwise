b8eda216a9bf5c931c75b51e9f89a0fe17802637
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-10-31 16:35:53 +0530

    changes

--- src/app/lead/personal-loan/view/page.tsx
+++ src/app/lead/personal-loan/view/page.tsx
@@ -1,6 +1,7 @@
 
-import { ChevronRightIcon, UserPlusIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon, PhoneIcon, UserPlusIcon } from "@heroicons/react/24/outline";
 import { NextPage } from "next";
+import Image from "next/image";
 const View: NextPage = () => {
     return (
         <>
@@ -8,98 +9,57 @@ const View: NextPage = () => {
                 <div className="text-black w-full bottom-[calc(100vh-theme(spacing.16))] left-0 right-0 top-0 sticky ">
                     <div className="flex justify-end  bg-slate-200 py-2 rounded-t px-3">
                         <div className="flex h-8 my-auto">
-                            <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
+                            <span  className="flex bg-gray-50 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
                                 <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d '}}/>
-                                <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Call Customer</span>
+                                <span className="text-sm md:text-xs font-normal my-auto" style={{color:'#12344d'}}>Call Customer</span>
                             </span>
-                            <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
+                            <span  className="flex bg-gray-50 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
                                 <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                                <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Email</span>
+                                <span className="text-sm md:text-xs font-normal my-auto" style={{color:'#12344d'}}>Email</span>
                             </span>
-                            <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
+                            <span  className="flex bg-gray-50 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
                                 <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                                <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Add Task</span>
+                                <span className="text-sm md:text-xs font-normal my-auto" style={{color:'#12344d'}}>Add Task</span>
                             </span>
-                            <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
+                            <span  className="flex bg-gray-50 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
                                 <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                                <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Share Link</span>
+                                <span className="text-sm md:text-xs font-normal my-auto" style={{color:'#12344d'}}>Share Link</span>
                             </span>
-                            <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
+                            <span  className="flex bg-gray-50 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
                                 <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                                <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Notify</span>
+                                <span className="text-sm md:text-xs font-normal my-auto" style={{color:'#12344d'}}>Notify</span>
                             </span>
-                            <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
+                            <span  className="flex bg-gray-50 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
                                 <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
-                                <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Check Eligibility</span>
+                                <span className="text-sm md:text-xs font-normal my-auto" style={{color:'#12344d'}}>Check Eligibility</span>
                             </span>
                         </div>
                     </div>
-                    {/* <div className="flex bg-slate-50 rounded-b py-6">
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Phone number</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>1234567890</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Emaiil Address</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>Admin@gmail.com</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Alt Phone number</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>1234567890</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Date of birth</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>30-12-2323</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">PAN number</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>ASFA1212A</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Gender</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>Male</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Marital Status</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>Single</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Aadhar Number</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>1234 1244 1234</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Mother Name</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>Single</span>
-                        </div>
-                        <div className="flex flex-col my-auto px-3">
-                            <span className="text-xs text-slate-500 pb-1 text-gray-900">Father Name</span>
-                            <span className="text-xs font-semibold" style={{color:'#12344d'}}>Single</span>
-                        </div>
-                    </div> */}
                 </div>
                 <div className="flex fixed h-screen text-black">
-                    <div className="sticky w-fit bg-gray-300 p-4 overflow-y-auto">
+                    <div className="sticky bg-gray-100 p-4 w-full">
                         {/* Left Side (Fixed) */}
                         <div className="overflow-y-auto">
-                        <h2 className="text-xl font-bold mb-4">Left Side (Fixed)</h2>
-                        <p className="text-black">
-                            On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-                            </p>
-                            <p className="text-black">
-                            On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-                            </p>
-                            <p className="text-black">
-                            On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-                            </p>
-                            <p className="text-black">
-                            On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-                            </p>
-                            <p className="text-black">
-                            On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-                            </p>
-                            <p className="text-black">
-                            On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-                            </p>
+                        <div className="text-black">
+                            <div className="grid border-b border-black-800">
+                                <div className=" grid pb-2">
+                                    <span className="font-semibold text-lg">Shivkumar Acharya</span>
+                                    <span className="text-xs">Business Development Manger</span>
+                                </div>
+                                <div className="grid py-3 gap-2">
+                                    <span className="text-xs flex" style={{color:'#2058dc'}}>
+                                    <PhoneIcon className="h-4 pr-2" color="#2058dc"/>
+                                        +91 1234567890
+                                    </span>
+                                    <span className="text-xs flex" style={{color:'#2058dc'}}>
+                                    <EnvelopeIcon className="h-4 pr-2" color="#2058dc"/>
+                                        acharyashivkumar67@gmail.com
+                                    </span>
+
+                                    {/* <span className="text-xs">1234567890</span> */}
+                                </div>
+                            </div>
+                        </div>
                         </div>
                     </div>
                     <div className="relative p-4">
