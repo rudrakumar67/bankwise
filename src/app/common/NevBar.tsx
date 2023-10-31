7399ee759102604c70235e4f1d251c41bdf79c44
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-10-31 12:18:38 +0530

    changes

--- src/app/common/NevBar.tsx
+++ src/app/common/NevBar.tsx
@@ -6,7 +6,7 @@ import {
   ArrowDownTrayIcon
  } from '@heroicons/react/24/outline'
 import { useState } from 'react';
-
+import {Tooltip} from "@nextui-org/tooltip";
 function Navbar() {
   // Import Data Modal
   const [importDataModal, setimportDataModal] = useState(false);
@@ -49,31 +49,59 @@ function Navbar() {
             </div>
           </div>
           <ul className="flex items-center flex-shrink-0 space-x-5">
-            <a href="" className='rounded my-auto'>
-              <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#00539c">
-                <g id="SVGRepo_bgCarrier" stroke-width="0"/>
-                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-                <g id="SVGRepo_iconCarrier"> 
-                  <path d="M7 5C7 6.10457 6.10457 7 5 7C3.89543 7 3 6.10457 3 5C3 3.89543 3.89543 3 5 3C6.10457 3 7 3.89543 7 5Z" fill="#00539c"/>
-                  <path d="M14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5Z" fill="#00539c"/>
-                  <path d="M19 7C20.1046 7 21 6.10457 21 5C21 3.89543 20.1046 3 19 3C17.8954 3 17 3.89543 17 5C17 6.10457 17.8954 7 19 7Z" fill="#00539c"/> 
-                  <path d="M7 12C7 13.1046 6.10457 14 5 14C3.89543 14 3 13.1046 3 12C3 10.8954 3.89543 10 5 10C6.10457 10 7 10.8954 7 12Z" fill="#00539c"/> 
-                  <path d="M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z" fill="#00539c"/> 
-                  <path d="M21 12C21 13.1046 20.1046 14 19 14C17.8954 14 17 13.1046 17 12C17 10.8954 17.8954 10 19 10C20.1046 10 21 10.8954 21 12Z" fill="#00539c"/> 
-                  <path d="M5 21C6.10457 21 7 20.1046 7 19C7 17.8954 6.10457 17 5 17C3.89543 17 3 17.8954 3 19C3 20.1046 3.89543 21 5 21Z" fill="#00539c"/> 
-                  <path d="M14 19C14 20.1046 13.1046 21 12 21C10.8954 21 10 20.1046 10 19C10 17.8954 10.8954 17 12 17C13.1046 17 14 17.8954 14 19Z" fill="#00539c"/> 
-                  <path d="M19 21C20.1046 21 21 20.1046 21 19C21 17.8954 20.1046 17 19 17C17.8954 17 17 17.8954 17 19C17 20.1046 17.8954 21 19 21Z" fill="#00539c"/> </g>
-              </svg>
-            </a>
-            <a href="" className='border rounded p-2 border-gray-200'>
-              <EnvelopeIcon className='h-4 w-4 text-blue-500'/>
-            </a>
-            <a href="" className='border rounded p-2 border-gray-200'>
-              <BellAlertIcon className='h-4 w-4 text-blue-500'/>
-            </a>
-            <a href="" className='rounded p-2' style={{backgroundColor:'#00539c', borderColor: '#00539c'}}>
-              <UserIcon className='h-4 w-4 text-slate-50'/>
-            </a>
+          <Tooltip
+              showArrow={false}
+              content="Tools" 
+              offset={7} 
+              closeDelay={0} 
+              classNames={{base: "text-white bg-theme rounded text-xs py-0.5 px-2 mt-0.5 m-0", arrow: "bg-blue-900 py-0.5 px-2 mt-0.5 m-0"}}>
+              <a href="" className='rounded my-auto'>
+                <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#00539c">
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+                  <g id="SVGRepo_iconCarrier"> 
+                    <path d="M7 5C7 6.10457 6.10457 7 5 7C3.89543 7 3 6.10457 3 5C3 3.89543 3.89543 3 5 3C6.10457 3 7 3.89543 7 5Z" fill="#00539c"/>
+                    <path d="M14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5Z" fill="#00539c"/>
+                    <path d="M19 7C20.1046 7 21 6.10457 21 5C21 3.89543 20.1046 3 19 3C17.8954 3 17 3.89543 17 5C17 6.10457 17.8954 7 19 7Z" fill="#00539c"/> 
+                    <path d="M7 12C7 13.1046 6.10457 14 5 14C3.89543 14 3 13.1046 3 12C3 10.8954 3.89543 10 5 10C6.10457 10 7 10.8954 7 12Z" fill="#00539c"/> 
+                    <path d="M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z" fill="#00539c"/> 
+                    <path d="M21 12C21 13.1046 20.1046 14 19 14C17.8954 14 17 13.1046 17 12C17 10.8954 17.8954 10 19 10C20.1046 10 21 10.8954 21 12Z" fill="#00539c"/> 
+                    <path d="M5 21C6.10457 21 7 20.1046 7 19C7 17.8954 6.10457 17 5 17C3.89543 17 3 17.8954 3 19C3 20.1046 3.89543 21 5 21Z" fill="#00539c"/> 
+                    <path d="M14 19C14 20.1046 13.1046 21 12 21C10.8954 21 10 20.1046 10 19C10 17.8954 10.8954 17 12 17C13.1046 17 14 17.8954 14 19Z" fill="#00539c"/> 
+                    <path d="M19 21C20.1046 21 21 20.1046 21 19C21 17.8954 20.1046 17 19 17C17.8954 17 17 17.8954 17 19C17 20.1046 17.8954 21 19 21Z" fill="#00539c"/> </g>
+                </svg>
+              </a>
+            </Tooltip>
+            <Tooltip
+              showArrow={false}
+              content="Emails" 
+              offset={7} 
+              closeDelay={0} 
+              classNames={{base: "text-white bg-theme rounded text-xs py-0.5 px-2 mt-0.5 m-0", arrow: "bg-blue-900 py-0.5 px-2 mt-0.5 m-0"}}>
+              <a href="" className='border rounded p-2 border-gray-200'>
+                <EnvelopeIcon className='h-4 w-4 text-blue-500'/>
+              </a>
+            </Tooltip>
+            <Tooltip
+              showArrow={false}
+              content="Notification" 
+              offset={7} 
+              closeDelay={0} 
+              classNames={{base: "text-white bg-theme rounded text-xs py-0.5 px-2 mt-0.5 m-0", arrow: "bg-blue-900 py-0.5 px-2 mt-0.5 m-0"}}>
+              <a href="" className='border rounded p-2 border-gray-200'>
+                <BellAlertIcon className='h-4 w-4 text-blue-500'/>
+              </a>
+            </Tooltip>
+            <Tooltip
+              showArrow={false}
+              content="Profile" 
+              offset={7} 
+              closeDelay={0} 
+              classNames={{base: "text-white bg-theme rounded text-xs py-0.5 px-2 mt-0.5 m-0", arrow: "bg-blue-900 py-0.5 px-2 mt-0.5 m-0"}}>
+              <a href="" className='rounded p-2' style={{backgroundColor:'#00539c', borderColor: '#00539c'}}>
+                <UserIcon className='h-4 w-4 text-slate-50'/>
+              </a>
+            </Tooltip>
           </ul>
         </div>
       </header>
--- src/app/lead/personal-loan/view/page.tsx
+++ src/app/lead/personal-loan/view/page.tsx
@@ -26,70 +26,70 @@ const View: NextPage = () => {
                     </div>
                     <div className="flex h-8 my-auto">
                         <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
-                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#00539c'}}/>
-                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#00539c'}}>Call Customer</span>
+                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d '}}/>
+                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Call Customer</span>
                         </span>
                         <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
-                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#00539c'}}/>
-                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#00539c'}}>Email</span>
+                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Email</span>
                         </span>
                         <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
-                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#00539c'}}/>
-                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#00539c'}}>Add Task</span>
+                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Add Task</span>
                         </span>
                         <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
-                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#00539c'}}/>
-                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#00539c'}}>Share Link</span>
+                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Share Link</span>
                         </span>
                         <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
-                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#00539c'}}/>
-                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#00539c'}}>Notify</span>
+                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Notify</span>
                         </span>
                         <span  className="flex bg-gray-100 text-gray-800 py-1 px-4 md:py-0.5 md:px-2 sec-btn-border rounded shadow mr-2 cursor-pointer" style={{borderColor:'#0e387a'}}>
-                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#00539c'}}/>
-                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#00539c'}}>Check Eligibility</span>
+                            <UserPlusIcon className={"h-4.5 w-4 mr-2"} style={{color:'#12344d'}}/>
+                            <span className="text-sm md:text-xs font-semibold my-auto" style={{color:'#12344d'}}>Check Eligibility</span>
                         </span>
                     </div>
                 </div>
                 <div className="flex bg-slate-50 rounded-b py-6">
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Phone number</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Phone number</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>1234567890</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Emaiil Address</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Emaiil Address</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>Admin@gmail.com</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Alt Phone number</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Alt Phone number</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>1234567890</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Date of birth</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Date of birth</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>30-12-2323</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>PAN number</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">PAN number</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>ASFA1212A</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Gender</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Gender</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>Male</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Marital Status</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Marital Status</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>Single</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Aadhar Number</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Aadhar Number</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>1234 1244 1234</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Mother Name</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Mother Name</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>Single</span>
                     </div>
                     <div className="flex flex-col my-auto px-3">
-                        <span className="text-xs text-slate-500 pb-1" style={{color:'#00539c'}}>Father Name</span>
+                        <span className="text-xs text-slate-500 pb-1 text-gray-900">Father Name</span>
                         <span className="text-xs font-semibold" style={{color:'#12344d'}}>Single</span>
                     </div>
                 </div>
