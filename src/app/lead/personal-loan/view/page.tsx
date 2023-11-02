1f7f87ad0a041fcd181d67bbe9fa19963e8492ba
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-11-02 11:57:48 +0530

    changes

--- src/app/lead/personal-loan/view/page.tsx
+++ src/app/lead/personal-loan/view/page.tsx
@@ -1,9 +1,12 @@
-
+'use client'
 import { ArrowsRightLeftIcon, BellAlertIcon, BuildingLibraryIcon, CalculatorIcon, ChartPieIcon, ChatBubbleBottomCenterIcon, DocumentDuplicateIcon, EnvelopeIcon, PencilIcon, PencilSquareIcon, PhoneIcon, QueueListIcon, UserCircleIcon, UserIcon, UserPlusIcon, UsersIcon } from "@heroicons/react/24/outline";
 import { Tooltip } from "@nextui-org/tooltip";
+// import {Accordion, AccordionItem} from "@nextui-org/accordion";
+import {Accordion, AccordionItem} from "@nextui-org/react";
 import { NextPage } from "next";
 import Image from "next/image";
 const View: NextPage = () => {
+    const defaultContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     return (
         <>
             <div className="bg-white sticky">
@@ -37,7 +40,7 @@ const View: NextPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex fixed text-black text-left">
+                <div className="flex text-black text-left">
                     <div className="w-72">
                         <div className="sticky bg-gray-100 p-4 w-full">
                             {/* Left Side (Fixed) */}
@@ -132,28 +135,113 @@ const View: NextPage = () => {
                         </div>    
                     </div>
                     <div className="w-10/12">
-                        <div className="p-4 w-full">
-                            <div className="h-screen overflow-y-auto">
-                                <div className="grid grid-cols-2 gap-4">
-                                    <div className="bg-gray-100 rounded">
-                                        <div className="text-sm font-semibold rounded p-2 bg-slate-200 flex justify-between">
-                                            <span>Heading</span>
-                                            <PencilIcon className="h-3.5 my-auto mx-1"/>
-                                        </div>
-                                        <p className="p-2">
-                                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                        <div className="">
+                            <div>
+                                <h2 className="sr-only">Steps</h2>
+
+                                <div>
+                                    <ol
+                                    className="grid grid-cols-1 divide-x divide-gray-100 overflow-hidden rounded-lg border border-gray-100 text-sm text-gray-500 sm:grid-cols-3"
+                                    >
+                                    <li className="flex items-center justify-center gap-2 p-4">
+                                        <svg
+                                        className="h-7 w-7 shrink-0"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+                                        />
+                                        </svg>
+
+                                        <p className="leading-none">
+                                        <strong className="block font-medium"> Details </strong>
+                                        <small className="mt-1"> Some info about you. </small>
                                         </p>
-                                    </div>
-                                    <div className="bg-gray-100 rounded">
-                                        <div className="text-sm font-semibold rounded p-2 bg-slate-200 flex justify-between">
-                                            <span>Heading</span>
-                                            <PencilIcon className="h-3.5 my-auto mx-1"/>
-                                        </div>
-                                        <p className="p-2">
-                                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                                    </li>
+
+                                    <li
+                                        className="relative flex items-center justify-center gap-2 bg-gray-50 p-4"
+                                    >
+                                        <span
+                                        className="absolute -left-2 top-1/2 hidden h-4 w-4 -translate-y-1/2 rotate-45 border border-gray-100 ltr:border-b-0 ltr:border-s-0 ltr:bg-white rtl:border-e-0 rtl:border-t-0 rtl:bg-gray-50 sm:block"
+                                        >
+                                        </span>
+
+                                        <span
+                                        className="absolute -right-2 top-1/2 hidden h-4 w-4 -translate-y-1/2 rotate-45 border border-gray-100 ltr:border-b-0 ltr:border-s-0 ltr:bg-gray-50 rtl:border-e-0 rtl:border-t-0 rtl:bg-white sm:block"
+                                        >
+                                        </span>
+
+                                        <svg
+                                        className="h-7 w-7 shrink-0"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                                        />
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                                        />
+                                        </svg>
+
+                                        <p className="leading-none">
+                                        <strong className="block font-medium"> Address </strong>
+                                        <small className="mt-1"> Where we sending it? </small>
                                         </p>
-                                    </div>
+                                    </li>
+
+                                    <li className="flex items-center justify-center gap-2 p-4">
+                                        <svg
+                                        className="h-7 w-7 shrink-0"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                                        />
+                                        </svg>
+
+                                        <p className="leading-none">
+                                        <strong className="block font-medium"> Payment </strong>
+                                        <small className="mt-1"> Show us the money. </small>
+                                        </p>
+                                    </li>
+                                    </ol>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="p-4">
+                            <div className="h-screen overflow-y-auto w-full">
+                            <Accordion isCompact>
+                                <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
+                                    {defaultContent}
+                                </AccordionItem>
+                                <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+                                {defaultContent}
+                                </AccordionItem>
+                                <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+                                {defaultContent}
+                                </AccordionItem>
+                            </Accordion>
                             </div>
                         </div>
                     </div>
